e7829473c96d7b9518d265d1a31a4d67b77564b1
nengakdakup <nengakdakup@gmail.com>
2020-05-04 23:39:41 +0100

    Updates

---
+++ precache-manifest.b4117f9b6e08f42b59e68c71bf99ff27.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9750ac4aa8573f6aac4dba6e59daed8c",
+    "url": "/covid-self-assesment/index.html"
+  },
+  {
+    "revision": "c4118bc9ca1950c7fe84",
+    "url": "/covid-self-assesment/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "54a6961bbaf40c774ea3",
+    "url": "/covid-self-assesment/static/css/main.3a1b807f.chunk.css"
+  },
+  {
+    "revision": "c4118bc9ca1950c7fe84",
+    "url": "/covid-self-assesment/static/js/2.e15d6eab.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/covid-self-assesment/static/js/2.e15d6eab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54a6961bbaf40c774ea3",
+    "url": "/covid-self-assesment/static/js/main.5a37a5f0.chunk.js"
+  },
+  {
+    "revision": "c473efa389e9886504fb",
+    "url": "/covid-self-assesment/static/js/runtime-main.1b3e956b.js"
+  },
+  {
+    "revision": "8f6c9146624684c002121f50df07ab81",
+    "url": "/covid-self-assesment/static/media/doctor.8f6c9146.svg"
+  }
+]);
